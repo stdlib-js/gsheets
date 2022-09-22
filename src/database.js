0112e8ff5deebe2143d852e474bbb55587e04bdc
Athan Reines <kgryte@gmail.com>
2022-09-22 02:40:30 -0700

    Rename and refactor alias database

---
+++ src/database.js
@@ -0,0 +1,116 @@
+/**
+* @license Apache-2.0
+*
+* Copyright (c) 2022 The Stdlib Authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+'use strict';
+
+// MAIN //
+
+/*
+* When adding names to the namespace, ensure that they are added in alphabetical order according to alias (namespace key).
+*/
+
+var ns = [];
+
+ns.push({
+	'alias': 'anscombes_quartet',
+	'pkg': '@stdlib/datasets/anscombes-quartet',
+	'related': []
+});
+
+ns.push({
+	'alias': 'bartlett_test',
+	'pkg': '@stdlib/stats/bartlett-test',
+	'related': []
+});
+
+ns.push({
+	'alias': 'blas.daxpy',
+	'pkg': '@stdlib/blas/base/gaxpy',
+	'related': []
+});
+
+ns.push({
+	'alias': 'blas.saxpy',
+	'pkg': '@stdlib/blas/base/gaxpy',
+	'related': []
+});
+
+ns.push({
+	'alias': 'cumax',
+	'pkg': '@stdlib/stats/base/cumax',
+	'related': []
+});
+
+ns.push({
+	'alias': 'kebabcase',
+	'pkg': '@stdlib/string/kebabcase',
+	'related': []
+});
+
+ns.push({
+	'alias': 'lowercase',
+	'pkg': '@stdlib/string/lowercase',
+	'related': []
+});
+
+ns.push({
+	'alias': 'random.minstd',
+	'pkg': '@stdlib/random/base/minstd',
+	'related': []
+});
+
+ns.push({
+	'alias': 'random.minstd_shuffle',
+	'pkg': '@stdlib/random/base/minstd-shuffle',
+	'related': []
+});
+
+ns.push({
+	'alias': 'random.mt19937',
+	'pkg': '@stdlib/random/base/mt19937',
+	'related': []
+});
+
+ns.push({
+	'alias': 'random.normal',
+	'pkg': '@stdlib/random/base/normal',
+	'related': []
+});
+
+ns.push({
+	'alias': 'random.uniform',
+	'pkg': '@stdlib/random/base/uniform',
+	'related': []
+});
+
+ns.push({
+	'alias': 'sawtooth_wave',
+	'pkg': '@stdlib/simulate/iter/sawtooth-wave',
+	'related': []
+});
+
+ns.push({
+	'alias': 'zeta',
+	'pkg': '@stdlib/math/base/special/riemann-zeta',
+	'related': []
+});
+
+
+// EXPORTS //
+
+module.exports = ns;
