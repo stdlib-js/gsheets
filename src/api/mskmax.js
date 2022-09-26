a51c7f87a2d07f7c9ede7e50ec47c890094f7945
Athan Reines <kgryte@gmail.com>
2022-09-26 00:52:48 -0700

    Build app

--- src/api/mskmax.js
+++ src/api/mskmax.js
@@ -30,6 +30,10 @@ var ns = require( './../namespace.js' );
 /**
 * Computes the maximum value according to a mask.
 *
+* ## Notes
+*
+* -
+*
 * @customfunction
 * @param {Range<number>} x - input range
 * @param {Range<integer>} mask - mask range
@@ -41,6 +45,9 @@ var ns = require( './../namespace.js' );
 * STDLIB_MSKMAX( A1:A100, B1:B100 )
 *
 * @example
+* STDLIB_MSKMAX( A1:C100, D1:D100 )
+*
+* @example
 * STDLIB_MSKMAX( A1:C100, D1:F100, "axis", 1 )
 *
 * @example
@@ -48,19 +55,24 @@ var ns = require( './../namespace.js' );
 */
 function STDLIB_MSKMAX( x, mask, axis, axisValue ) { // eslint-disable-line no-unused-vars
 	var sarray;
-	var offset;
 	var smask;
 	var out;
+	var som;
 	var ax;
+	var sm;
+	var ox;
+	var om;
 	var M;
 	var N;
 	var o;
 	var v;
 	var i;
+	var j;
 
 	ns.assert.isRange( x, 'First argument' );
 	ns.assert.isRange( mask, 'Second argument' );
-	ns.assert.isSameShape( x, mask, 'First and second arguments' );
+	ns.assert.isBroadcastCompatibleWith( x, mask, 'Second argument', 'first argument' );
+
 	ax = 1;
 	for ( i = 2; i < arguments.length; i += 2 ) {
 		o = arguments[ i ];
@@ -78,8 +90,18 @@ function STDLIB_MSKMAX( x, mask, axis, axisValue ) { // eslint-disable-line no-u
 	if ( ax === 0 ) {
 		M = x.length;
 		N = x[ 0 ].length;
+
+		// Check whether we need to broadcast the mask array...
+		if ( mask.length === 1 ) {
+			// Set the mask "stride" to zero to always use the same mask array:
+			som = 0;
+		} else {
+			som = 1;
+		}
+		j = 0;
 		for ( i = 0; i < M; i++ ) {
-			out.push( ns.mskmax( N, x[ i ], 1, 0, mask[ i ], 1, 0 ) );
+			out.push( ns.mskmax( N, x[ i ], 1, 0, mask[ j ], 1, 0 ) );
+			j += som;
 		}
 		return out;
 	}
@@ -91,13 +113,23 @@ function STDLIB_MSKMAX( x, mask, axis, axisValue ) { // eslint-disable-line no-u
 	sarray = ns.flattenArray( x );
 	smask = ns.flattenArray( mask );
 
-	// Set the offset which defines the pointer to the first indexed element in a column:
-	offset = 0;
+	// Check whether we need to broadcast the mask array...
+	if ( mask[ 0 ].length === 1 ) {
+		sm = 1;  // mask array stride
+		som = 0; // offset stride for mask array
+	} else {
+		sm = M;  // multiple columns, so need to specify an increment to index column values
+		som = 1; // multiple columns, so need to specify an increment to locate the first indexed value for a column
+	}
+	// Set the offsets which define the pointers to the first indexed element in a column:
+	ox = 0;
+	om = 0;
 
 	// Iterate over rows...
 	for ( i = 0; i < M; i++ ) {
-		out.push( ns.mskmax( N, sarray, M, offset, smask, M, offset ) );
-		offset += 1;
+		out.push( ns.mskmax( N, sarray, M, ox, smask, sm, om ) );
+		ox += 1;
+		om += som;
 	}
 	return [ out ];
 }
