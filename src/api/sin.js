f7f423b52d1fdf84b907287de88e5e9d4addc145
Stephannie Jimenez <steff456@hotmail.com>
2023-09-15 14:54:37 -0500

    feat: add function for computing sine values

---
+++ src/api/sin.js
@@ -0,0 +1,89 @@
+/**
+* @license Apache-2.0
+*
+* Copyright (c) 2023 The Stdlib Authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+/* This is a generated file. Do not edit directly. */
+
+/* eslint-disable stdlib/jsdoc-doctest-marker, stdlib/jsdoc-doctest-quote-props */
+
+'use strict';
+
+// MODULES //
+
+var ns = require( './../namespace' );
+
+
+// MAIN //
+
+/**
+* Computes the sine of a number (in radians).
+*
+* @customfunction
+* @param {number|Range<number>} value - input value(s)
+* @param {string} nonnumeric - option name for specifying the value to return in place of raising an exception when an input value is not a number
+* @param {*} nonnumericValue - non-numeric option value
+* @param {string} nan - option name for specifying the value to return in place of `NaN`
+* @param {*} nanValue - `NaN` option value
+* @param {string} pinf - option name for specifying the value to return in place of positive infinity
+* @param {*} pinfValue - positive infinity option value
+* @param {string} ninf - option name for specifying the value to return in place of negative infinity
+* @param {*} ninfValue - negative infinity option value
+* @returns {number|Range<number>} result(s)
+*
+* @example
+* STDLIB_SIN( 0.0 )
+*
+* @example
+* STDLIB_SIN( A1:A100 )
+*
+* @example
+* STDLIB_SIN( A1:D100 )
+*
+* @example
+* STDLIB_SIN( A1:A100, "nan", "", "pinf", "", "ninf", "" )
+*/
+function STDLIB_SIN( value, nonnumeric, nonnumericValue, nan, nanValue, pinf, pinfValue, ninf, ninfValue ) { // eslint-disable-line no-unused-vars
+	var opts;
+	var f;
+	var o;
+	var i;
+
+	opts = {
+		'nonnumeric': void 0,
+		'nan': NaN,
+		'pinf': Infinity,
+		'ninf': -Infinity
+	};
+	for ( i = 1; i < arguments.length; i += 2 ) {
+		o = arguments[ i ];
+		if ( o === 'nonnumeric' || o === 'nan' || o === 'pinf' || o === 'ninf' ) {
+			opts[ o ] = arguments[ i+1 ];
+		} else {
+			ns.assert.unrecognizedOptionName( o );
+		}
+	}
+	f = ns.tools.d_d( ns.sin, opts );
+	if ( ns.isArray( value ) ) {
+		return ns.tools.unary2d( value, f );
+	}
+	return f( value );
+}
+
+
+// EXPORTS //
+
+module.exports = STDLIB_SIN;
