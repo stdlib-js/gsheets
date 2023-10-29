102d528022fe9a4fca0b48c3f762df2fbc1e8f75
Athan Reines <kgryte@gmail.com>
2023-10-29 14:43:48 -0700

    Refactor to use math tooling

--- src/api/dists_normal_kurtosis.js
+++ src/api/dists_normal_kurtosis.js
@@ -25,12 +25,6 @@
 var ns = require( './../namespace' );
 
 
-// VARIABLES //
-
-var __STDLIB_DISTS_NORMAL_KURTOSIS_DTYPES = [ 'number', 'number' ]; // eslint-disable-line no-underscore-dangle
-var __STDLIB_DISTS_NORMAL_KURTOSIS_MSGS = [ 'First argument', 'Second argument' ]; // eslint-disable-line no-underscore-dangle
-
-
 // MAIN //
 
 /**
@@ -63,47 +57,14 @@ var __STDLIB_DISTS_NORMAL_KURTOSIS_MSGS = [ 'First argument', 'Second argument' 
 * STDLIB_DISTS_NORMAL_KURTOSIS( 1, B1:B100, "nan", "", "pinf", "", "ninf", "" )
 */
 function STDLIB_DISTS_NORMAL_KURTOSIS( mu, sigma, nonnumeric, nonnumericValue, nan, nanValue, pinf, pinfValue, ninf, ninfValue ) { // eslint-disable-line no-unused-vars, max-len
-	var shape;
 	var args;
-	var opts;
-	var out;
-	var f;
-	var o;
 	var i;
 
-	opts = {
-		'nonnumeric': void 0,
-		'nan': NaN,
-		'pinf': Infinity,
-		'ninf': -Infinity
-	};
-	for ( i = 2; i < arguments.length; i += 2 ) {
-		o = arguments[ i ];
-		if ( o === 'nonnumeric' || o === 'nan' || o === 'pinf' || o === 'ninf' ) {
-			opts[ o ] = arguments[ i+1 ];
-		} else {
-			ns.assert.unrecognizedOptionName( o );
-		}
+	args = [];
+	for ( i = 0; i < arguments.length; i++ ) {
+		args.push( arguments[ i ] );
 	}
-	// Normalize the provided arguments so we are always working with nested arrays:
-	args = ns.normalizeBroadcastArgs( [ mu, sigma ], __STDLIB_DISTS_NORMAL_KURTOSIS_DTYPES, __STDLIB_DISTS_NORMAL_KURTOSIS_MSGS ); // eslint-disable-line max-len
-
-	// Resolve the shape of the broadcasted result:
-	shape = ns.broadcastShapes( args[ 1 ] );
-	if ( shape === null ) {
-		throw new Error( 'invalid argument. Input arguments are not broadcast compatible.' );
-	}
-	// Create an output array:
-	out = ns.zeros2d( shape );
-
-	// Wrap the lower-level function to ensure proper handling of input arguments and return values:
-	f = ns.tools.dd_d( ns.dists.normal.kurtosis, opts );
-
-	// Apply the function to broadcasted arrays:
-	args[ 0 ].push( out );
-	args[ 1 ].push( shape );
-	ns.tools.bbinary2d( args[ 0 ], args[ 1 ], f );
-	return out;
+	return ns.math.tools.binary( args, ns.dists.normal.kurtosis );
 }
 
 
